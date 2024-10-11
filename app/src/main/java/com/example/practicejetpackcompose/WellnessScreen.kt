387459c44307ab606bfbe4a6cc1dc20749aac981
almuheetushihab <almuheetu@gmail.com>
2024-10-11 16:22:50 +0600

    completely making counter

--- app/src/main/java/com/example/practicejetpackcompose/WellnessScreen.kt
+++ app/src/main/java/com/example/practicejetpackcompose/WellnessScreen.kt
@@ -2,8 +2,10 @@ package com.example.practicejetpackcompose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier){
     WaterCounter(modifier)
 }
+
