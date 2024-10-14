024be9fea0ab8b727002fb4590c8d0a922851971
almuheetushihab <almuheetu@gmail.com>
2024-10-14 10:38:23 +0600

    try to chekBox implementation

--- app/src/main/java/com/example/practicejetpackcompose/WellnessScreen.kt
+++ app/src/main/java/com/example/practicejetpackcompose/WellnessScreen.kt
@@ -2,7 +2,7 @@ package com.example.practicejetpackcompose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier){
--- app/src/main/java/com/example/practicejetpackcompose/WellnessTaskItem.kt
+++ app/src/main/java/com/example/practicejetpackcompose/WellnessTaskItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -16,6 +17,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ){
@@ -23,8 +26,14 @@ fun WellnessTaskItem(
         modifier = modifier, verticalAlignment  = Alignment.CenterVertically
     ){
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(onClick = onClose) {
             Icon(
@@ -38,5 +47,5 @@ fun WellnessTaskItem(
 @Preview(showBackground = true)
 @Composable
 fun WellnessTaskItemPreview() {
-    WellnessTaskItem(taskName = "This is a task", onClose = { })
+    WellnessTaskItem(taskName = "This is a task", onClose = { }, checked = true, onCheckedChange = {})
 }
